cd97fe579c79811f631acea71fdfed27729eddb0
wilnedthewilbert <125500672+wilnedthewilbert@users.noreply.github.com>
2023-02-15 13:58:32 -0500

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace riddle_game__wilned__gavin_ching__gabriel_flores_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.titlelbl = new System.Windows.Forms.Label();
+            this.exitbtn = new System.Windows.Forms.Button();
+            this.helpbtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // titlelbl
+            // 
+            this.titlelbl.AutoSize = true;
+            this.titlelbl.Font = new System.Drawing.Font("Lucida Sans Unicode", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titlelbl.Location = new System.Drawing.Point(271, 9);
+            this.titlelbl.Name = "titlelbl";
+            this.titlelbl.Size = new System.Drawing.Size(212, 39);
+            this.titlelbl.TabIndex = 0;
+            this.titlelbl.Text = "Riddle Game";
+            // 
+            // exitbtn
+            // 
+            this.exitbtn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.exitbtn.Location = new System.Drawing.Point(12, 9);
+            this.exitbtn.Name = "exitbtn";
+            this.exitbtn.Size = new System.Drawing.Size(34, 23);
+            this.exitbtn.TabIndex = 1;
+            this.exitbtn.Text = "exit";
+            this.exitbtn.UseVisualStyleBackColor = true;
+            this.exitbtn.Click += new System.EventHandler(this.exitbtn_Click);
+            // 
+            // helpbtn
+            // 
+            this.helpbtn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.helpbtn.Location = new System.Drawing.Point(21, 415);
+            this.helpbtn.Name = "helpbtn";
+            this.helpbtn.Size = new System.Drawing.Size(58, 23);
+            this.helpbtn.TabIndex = 2;
+            this.helpbtn.Text = "Help";
+            this.helpbtn.UseVisualStyleBackColor = true;
+            this.helpbtn.Click += new System.EventHandler(this.helpbtn_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.helpbtn);
+            this.Controls.Add(this.exitbtn);
+            this.Controls.Add(this.titlelbl);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label titlelbl;
+        private System.Windows.Forms.Button exitbtn;
+        private System.Windows.Forms.Button helpbtn;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace riddle_game__wilned__gavin_ching__gabriel_flores_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            MessageBox.Show("welcome to our riddle game! To begin you must select a dificulty on the right hand side and press begin, type your answer in the text box( it's only one word for all of them and lowercase) and then press enter. a message box while appear and tell you if you're right or wrong, get it wrong three times and the program shuts down :) Goooood luck!");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void exitbtn_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        
+
+        private void helpbtn_Click_1(object sender, EventArgs e)
+        {
+            MessageBox.Show("welcome to our riddle game! To begin you must select a dificulty on the right hand side and press begin, type your answer in the text box( it's only one word for all of them and lowercase) and then press enter. a message box while appear and tell you if you're right or wrong, get it wrong three times and the program shuts down :) Goooood luck!");
+        }
+    }
+}
